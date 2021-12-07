9faf87958bd9c0cfc2f8d78c5cf29d2156149962
Travis Howse <tjhowse@gmail.com>
2021-12-07 22:05:51 +1000

    Try a faster version

--- day6/day.go
+++ day6/day.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"strings"
+	"time"
 
 	tj "github.com/tjhowse/tjgo"
 )
@@ -24,12 +25,13 @@ func (f *fish) tick() bool {
 func main() {
 
 	mainPart1()
-	// mainPart2()
+	// mainPart1v2()
 }
 
 func mainPart1() {
 	a := tj.FileToSlice("input")
 
+	start := time.Now()
 	f := []fish{}
 
 	s := strings.Split(a[0], ",")
@@ -57,4 +59,42 @@ func mainPart1() {
 		lenSeq = append(lenSeq, lenSeq[i-1]+(lenSeq[i-b-1]-lenSeq[i-b-2])+(lenSeq[i-q-1]-lenSeq[i-q-2]))
 	}
 	println("Part 2:", lenSeq[len(lenSeq)-1])
+	end := time.Now()
+	println("Elapsed: ", end.Sub(start).Microseconds())
+}
+
+func mainPart1v2() {
+	a := tj.FileToSlice("input")
+
+	start := time.Now()
+
+	f := []fish{}
+
+	s := strings.Split(a[0], ",")
+	for _, i := range s {
+		f = append(f, fish{c: tj.Str2int(i)})
+	}
+	lenSeq := []int{}
+
+	for i := 0; i < 10; i++ {
+		newFish := []fish{}
+		for j := 0; j < len(f); j++ {
+			if f[j].tick() {
+				newFish = append(newFish, fish{c: 8})
+			}
+		}
+		f = append(f, newFish...)
+		lenSeq = append(lenSeq, len(f))
+	}
+	// println("Part 1:", len(f)) // Not accurate since we're not running for the full 80 days
+
+	b := 8
+	q := 6
+	for i := len(lenSeq); i < 256; i += 1 {
+		lenSeq = append(lenSeq, lenSeq[i-1]+(lenSeq[i-b-1]-lenSeq[i-b-2])+(lenSeq[i-q-1]-lenSeq[i-q-2]))
+	}
+	println("Part 2:", lenSeq[len(lenSeq)-1])
+
+	end := time.Now()
+	println("Elapsed: ", end.Sub(start).Microseconds())
 }
