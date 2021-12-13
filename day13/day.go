db1b3c734f417f5029fbdb5f7a1463d4ac344ca5
Travis Howse <tjhowse@gmail.com>
2021-12-13 19:06:19 +1000

    What a shitfest.

--- day13/day.go
+++ day13/day.go
@@ -30,12 +30,19 @@ func (s *sheet) setSize(x, y int) {
 			count++
 		}
 	}
+
+	println("s.maxX: ", s.maxX)
+	println("s.maxY: ", s.maxY)
+	println("len X: ", len(s.s[0]))
+	println("len Y: ", len(s.s))
 	println("count: ", count)
 }
 
 func (s *sheet) print() {
 	for i := 0; i <= s.maxY; i++ {
 		for j := 0; j <= s.maxX; j++ {
+			// for i := 0; i <= 14; i++ {
+			// for j := 0; j <= 10; j++ {
 			if s.s[i][j] {
 				print("#")
 			} else {
@@ -52,6 +59,9 @@ func (s *sheet) mark(x, y int) {
 	s.s[y][x] = true
 }
 func (s *sheet) count() int {
+	println("Counting:")
+	println("s.maxX:", s.maxX)
+	println("s.maxY:", s.maxY)
 	c := 0
 	for i := 0; i <= s.maxY; i++ {
 		for j := 0; j <= s.maxX; j++ {
@@ -65,40 +75,20 @@ func (s *sheet) count() int {
 
 }
 
-func (s *sheet) fold(x, y int) {
-	if x == 0 {
-		println("Folding along y:", y)
-		for i := y + 1; i <= s.maxY; i++ {
-			for j := 0; j <= s.maxX; j++ {
-				s.s[s.maxY-i][j] = s.s[i][j] || s.s[s.maxY-i][j]
-				s.s[i][j] = false
-			}
-		}
-		s.maxY = y - 1
-	} else {
-		println("Folding along x:", x)
-		for i := 0; i <= s.maxY; i++ {
-			for j := x + 1; j <= s.maxX; j++ {
-				s.s[i][s.maxX-j] = s.s[i][j] || s.s[i][s.maxX-j]
-				s.s[i][j] = false
-			}
-		}
-		s.maxX = x - 1
-	}
-}
-
 func (s *sheet) fold2(x, y int) {
 	if x != 0 {
 		for j := 0; j <= s.maxY; j++ {
-			for i := 0; i <= s.maxX; i++ {
+			for i := 0; i < x; i++ {
 				s.s[j][i] = s.s[j][i] || s.s[j][s.maxX-i]
+				s.s[j][s.maxX-i] = false
 			}
 		}
 		s.maxX = x - 1
 	} else {
-		for j := 0; j <= s.maxY; j++ {
+		for j := 0; j < y; j++ {
 			for i := 0; i <= s.maxX; i++ {
 				s.s[j][i] = s.s[j][i] || s.s[s.maxY-j][i]
+				s.s[s.maxY-j][i] = false
 			}
 		}
 		s.maxY = y - 1
@@ -149,10 +139,154 @@ func run(filename string) sheet {
 	return s
 }
 
+type better struct {
+	s map[[2]int]bool
+}
+
+func (b *better) init() {
+	b.s = make(map[[2]int]bool)
+}
+
+func (b *better) set(x, y int) {
+	b.s[[2]int{x, y}] = true
+}
+func (b *better) count() int {
+	c := 0
+	for _, v := range b.s {
+		if v {
+			c++
+		}
+	}
+	return c
+}
+func (b *better) flip(x, y int) {
+	if x != 0 {
+		// flipping on x
+		for k, v := range b.s {
+			if !v || k[0] < x {
+				continue
+			}
+			b.s[k] = false
+			rightOfSplit := k[0] - x
+			k[0] = x - rightOfSplit
+			b.s[k] = true
+		}
+	} else {
+		// flipping on y
+		for k, v := range b.s {
+			if !v || k[1] < y {
+				continue
+			}
+			b.s[k] = false
+			downOfSplit := k[1] - y
+			k[1] = y - downOfSplit
+			b.s[k] = true
+		}
+	}
+}
+func (b *better) draw() {
+	var mX, mY int
+
+	for k, v := range b.s {
+		if !v {
+			continue
+		}
+		if k[0] > mX {
+			mX = k[0]
+		}
+		if k[1] > mY {
+			mY = k[1]
+		}
+	}
+	for y := 0; y <= mY; y++ {
+		for x := 0; x <= mX; x++ {
+			if b.s[[2]int{x, y}] {
+				print("#")
+			} else {
+				print(" ")
+
+			}
+		}
+		println()
+	}
+	println("--------------------")
+}
+
+func run2(filename string) better {
+	a := tj.FileToSlice(filename)
+	rand.Seed(time.Now().UnixNano())
+
+	dotsList := []spot{}
+	// dotSet := map[spot]bool{}
+	hello := better{}
+	hello.init()
+
+	var maxX, maxY int
+
+	for _, a := range a {
+		// println(a)
+		if a == "" {
+			break
+		}
+		split := strings.Split(a, ",")
+		var s spot
+		s.x = tj.Str2int(split[0])
+		s.y = tj.Str2int(split[1])
+		if s.x > maxX {
+			maxX = s.x
+		}
+		if s.y > maxY {
+			maxY = s.y
+		}
+		dotsList = append(dotsList, s)
+		hello.set(s.x, s.y)
+		// dotSet[s] = true
+	}
+	return hello
+}
+
 func mainPart1() {
+	hello := run2("input")
+
+	println(hello.count())
+	hello.draw()
+	hello.flip(0, 7)
+	hello.draw()
+	hello.flip(5, 0)
+	hello.draw()
+	println(hello.count())
+	hello = run2("input_real")
+
+	println(hello.count())
+	hello.flip(655, 0)
+	hello.flip(0, 447)
+	hello.flip(327, 0)
+	hello.flip(0, 223)
+	hello.flip(163, 0)
+	hello.flip(0, 111)
+	hello.flip(81, 0)
+	hello.flip(0, 55)
+	hello.flip(40, 0)
+	hello.flip(0, 27)
+	hello.flip(0, 13)
+	hello.flip(0, 6)
+	hello.draw()
+
+	println(hello.count())
+	// for k, v := range dotSet {
+	// 	println("k: ", k.x, ",", k.y, " v: ", v)
+	// }
+
+	// return s
+	return
 	s := run("input")
 
+	// s.mark(5, 4)
+	// s.mark(5, 5)
+	// s.mark(5, 6)
+	// s.mark(8, 4)
 	s.print()
+	return
 	s.fold2(0, 7)
 	s.print()
 	s.fold2(5, 0)
@@ -160,6 +294,7 @@ func mainPart1() {
 	println(s.count())
 
 	println()
+	return
 
 	s = run("input_real")
 	println("maxX: ", s.maxX, "maxY: ", s.maxY)
